686a011efde5b05db736dc8505ddd3598295bfb7
raday146 <57037365+raday146@users.noreply.github.com>
2021-09-23 13:25:18 +0300

    test6

--- src/MovikoApp.js
+++ src/MovikoApp.js
@@ -33,28 +33,29 @@ const localStorage = window.localStorage;
 
 
 function MovikoApp(props){
+  const history = useHistory();
   let spin = !localStorage.getItem('cinema')? true : false;
   const [loading, spinner] = useSpinnerState(spin);
 
   const {classes} = props;
-  GetMoviesPages().map(movies => !cinema.allMovies.includes(movies)?  cinema.allMovies.push(movies): movies);
+ /* GetMoviesPages().map(movies => !cinema.allMovies.includes(movies)?  cinema.allMovies.push(movies): movies);
   GetMoviesByQuery(theNewst).map(movies => !cinema.newMovie.includes(movies)?  cinema.newMovie.push(movies): movies);
   GetMoviesByQuery(highestReted).map(movie=> !cinema.RetedMovie.includes(movie)? cinema.RetedMovie.push(movie):movie); 
   GetMoviesByQuery(populary).map(movies => !cinema.popularMovie.includes(movies)?  cinema.popularMovie.push(movies): movies);
-  
+  */
  useEffect(()=>{
     if(loading){
      setTimeout(()=>{
       
          const data =[
-                {id:"allMovies", allMovies:cinema.allMovies},
-                {id:"newMovie", newMovie:cinema.newMovie},
-                {id:"RetedMovie", RetedMovie:cinema.RetedMovie},
-                {id:"popularMovie", popularMovie:cinema.popularMovie}
+                {id:"allMovies", allMovies:Hmovies.allMovies},
+                {id:"newMovie", newMovie:Nmovies.newMovie},
+                {id:"RetedMovie", RetedMovie:Rmovies.RetedMovie},
+                {id:"popularMovie", popularMovie:Pmovies.popularMovie}
 
         ]
         
-        //console.log(data);
+        console.log(data);
          window.localStorage.setItem('cinema',JSON.stringify(data));
          //window.localStorage.setItem("highestReted", JSON.stringify(movieList));
          spinner();
